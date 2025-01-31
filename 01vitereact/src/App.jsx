c8a21263cc3fea5996031ccca3752958972c01c6
Shekhar <shekharsudhanshu801@gmail.com>
2025-02-01 01:13:59 +0530

    made customreact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,23 @@
+
+import Chai from "./chai"
+
+
+
+function App() {
+    const username="chai aur code"
+
+  return (
+    <>
+    <h1>Helloo {username}//yha pe evaluated expression hi likhte hain</h1>
+       <Chai/>
+
+    <p>chaii piloo friends</p>
+    
+    </>
+
+      
+    
+  )
+}
+
+export default App
